c15e60bbaededabfd70d06a760a82e0ae6739f25
aesperer <piyrw9754@gmail.com>
2024-03-14 10:16:22 +0900

    project init

---
+++ src/main/kotlin/esperer/concurrency/ConcurrencyApplication.kt
@@ -0,0 +1,11 @@
+package esperer.concurrency
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConcurrencyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConcurrencyApplication>(*args)
+}
---
+++ src/test/kotlin/esperer/concurrency/ConcurrencyApplicationTests.kt
@@ -0,0 +1,13 @@
+package esperer.concurrency
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
