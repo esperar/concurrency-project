6f83696d827043d42a036096790061e472788793
aesperer <piyrw9754@gmail.com>
2024-03-19 10:17:36 +0900

    create grpc service

--- src/main/kotlin/esperer/concurrency/grpc/GrpcServerService.kt
+++ src/main/kotlin/esperer/concurrency/grpc/GrpcServerService.kt
@@ -1,13 +1,26 @@
 package esperer.concurrency.grpc
 
-import esperer.concurrency.user.dto.UserRequest
-import esperer.concurrency.user.dto.UserResponse
+import com.google.protobuf.Int64Value
 import esperer.concurrency.user.service.UserQueryService
+import esperer.lib.ReactorUserQueryServiceGrpc.UserQueryServiceImplBase
+import esperer.lib.UserResponse
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 
-class GrpcServerService : UserQueryService {
-    override fun getUserInfo(request: Mono<UserRequest>): Mono<UserResponse> {
-        TODO("Not yet implemented")
+@Service
+class GrpcServerService(
+    private val userQueryService: UserQueryService
+) : UserQueryServiceImplBase(){
+
+    override fun getUserInfo(request: Mono<Int64Value>?): Mono<UserResponse> {
+        userQueryService.getUserInfo(request.map { it.value })
+            .map { it.toGrpc() }
     }
+
+    private fun esperer.concurrency.user.dto.UserResponse.toGrpc() =
+        UserResponse.newBuilder()
+            .setId(id)
+            .setName(name)
+            .build()
 }
